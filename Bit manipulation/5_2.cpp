9e23d7a376df3f6c056f2ac2e67617ccecbff14c
Eddy <adigupta239@gmail.com>
2022-02-26 19:01:08 +0530

    decimal to binary question

--- Bit manipulation/5_2.cpp
+++ Bit manipulation/5_2.cpp
@@ -1,3 +1,10 @@
+/*------------------------- Question ---------------------------
+Binary to String: Given a real number between O and 1 (e.g., 0.72) that is passed in as a double, print 
+the binary representation. If the number cannot be represented accurately in binary with at most 32 
+characters, print  "ERROR:' 
+------------------------------------------------------------------*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
