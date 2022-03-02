c39948969c64ca1e4ff3dde8db4b517971e2212b
Eddy <adigupta239@gmail.com>
2022-03-03 00:00:07 +0530

    fixed the code

--- Bit manipulation/5_3.cpp
+++ Bit manipulation/5_3.cpp
@@ -1,16 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void decToBinary(int n)
+{
+    // array to store binary number
+    int binaryNum[32];
+ 
+    // counter for binary array
+    int i = 0;
+    while (n > 0) {
+ 
+        // storing remainder in binary array
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+ 
+    // printing binary array in reverse order
+    for (int j = i - 1; j >= 0; j--)
+        cout << binaryNum[j];
+    cout<<"\n";
+}
+ 
+
 int max_sequence(int n){
+    decToBinary(n);
+    int max_count = 0;
     int count= 0;
     while(n > 0){
         if(n&1)
             count++;
         else 
             count = 0;
+        max_count = max(max_count,count);
         n = n >> 1;
     }
-    return count;
+    return max_count;
 }
 
 int main()
@@ -26,6 +51,7 @@ int main()
         if(!(temp & 1)){
             cout<<"Found a zero \n";
             int t = max_sequence(n^(1<<count));
+            cout<<t<<"\n";
             maximum = t > maximum ? t : maximum;
         }
         temp = temp >> 1;
